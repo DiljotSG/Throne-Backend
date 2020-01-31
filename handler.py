bddd132318020eac4fd70af7fd613e9e96031bf0
Diljot Garcha <diljotsg@gmail.com>
2020-01-30 19:55:25 -0600

    Update to use flask json function when returning dict

--- handler.py
+++ handler.py
@@ -1,12 +1,12 @@
-import json
-
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def washrooms():
-    return json.dumps({
+    return jsonify({
         "washrooms": [
             "Tache Hall",
             "6th Floor E2",
