463e65055b8e6580fd336d7f1355f078bc20ece3
Diljot Garcha <diljotsg@gmail.com>
2020-01-31 20:38:25 -0600

    Finalize new structure

---
+++ api/washrooms.py
@@ -0,0 +1,16 @@
+class Washroom:
+    def get_list(self):
+        return [
+                "Tache Hall",
+                "6th Floor E2",
+                "Aaron's House",
+                "151 Research",
+                "University Center",
+                "McDonald's Kenaston",
+                "3rd Floor Science and Technology Library",
+                "Wendy's Kenaston",
+                "Armes Tunnel Level",
+                "Eric's House",
+                "Tyler's House",
+                "University College Tunnel Level"
+            ]
