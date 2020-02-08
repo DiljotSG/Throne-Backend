f863f2564e41cc3d3a0f501922b074b4eb59ec43
Tyler Loewen <tylerscottloewen@gmail.com>
2020-02-08 10:58:22 -0600

    Generalize fetching of washroom list

--- api/blueprint.py
+++ api/blueprint.py
@@ -3,14 +3,26 @@ from flask import Blueprint
 from flask_cors import CORS
 from flask_cors import cross_origin
 
-from .objects.washroom import Washroom
-
 main_api = Blueprint('admin', __name__)
 cors = CORS(main_api)
-washroom = Washroom(0, "washroom", "EITC 2", "0", "male", 1, 1, 5)
 
 
 @main_api.route("/")
 @cross_origin()
 def get_washroom_list():
-    return jsonify(washroom.title, washroom.location, washroom.gender)
+    my_list = [
+        "Tache Hall",
+        "6th Floor E2",
+        "Aaron's House",
+        "151 Research",
+        "University Center",
+        "McDonald's Kenaston",
+        "3rd Floor Science and Technology Library",
+        "Wendy's Kenaston",
+        "Armes Tunnel Level",
+        "Eric's House",
+        "Tyler's House",
+        "University College Tunnel Level"
+    ]
+
+    return jsonify(my_list)
