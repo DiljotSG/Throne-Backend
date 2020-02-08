e218c050a8fe4b89dcd9a962788a6dfc2f58ea52
Aaron Roller <AaronMichaelRoller@gmail.com>
2020-02-08 13:11:12 -0600

    Seperated each endpoint route into their own file

---
+++ api/routes/washrooms.py
@@ -0,0 +1,40 @@
+from flask import jsonify
+from flask import Blueprint
+from flask_cors import CORS
+from flask_cors import cross_origin
+
+mod = Blueprint('washrooms', __name__)
+cors = CORS(mod)
+
+
+@mod.route("/")
+@cross_origin()
+def washrooms():
+    my_list = [
+        "Tache Hall",
+        "6th Floor E2",
+        "Aaron's House",
+        "151 Research",
+        "University Center",
+        "McDonald's Kenaston",
+        "3rd Floor Science and Technology Library",
+        "Wendy's Kenaston",
+        "Armes Tunnel Level",
+        "Eric's House",
+        "Tyler's House",
+        "University College Tunnel Level"
+    ]
+
+    return jsonify(my_list)
+
+
+@mod.route("/<int:washroom_id>")
+@cross_origin()
+def washrooms_id(washroom_id):
+    return jsonify({"msg": "washroom: " + str(washroom_id)})
+
+
+@mod.route("/<int:washroom_id>/reviews")
+@cross_origin()
+def washrooms_reviews(washroom_id):
+    return jsonify({"msg": "reviews for washroom: " + str(washroom_id)})
--- handler.py
+++ handler.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from api.blueprint import api
+
+from api.routes.washrooms import mod as washrooms_mod
+from api.routes.buildings import mod as buildings_mod
+from api.routes.reviews import mod as reviews_mod
+from api.routes.users import mod as users_mod
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.register_blueprint(api, url_prefix='/')
+app.register_blueprint(washrooms_mod, url_prefix="/washrooms")
+app.register_blueprint(buildings_mod, url_prefix="/buildings")
+app.register_blueprint(reviews_mod, url_prefix="/reviews")
+app.register_blueprint(users_mod, url_prefix="/users")
+
 
 def main():
     app.run()
+
 
 if __name__ == "__main__":
     main()
