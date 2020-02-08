6510bf459ea1302e167c85efc5f7340364317ce8
Tyler Loewen <tylerscottloewen@gmail.com>
2020-02-08 11:10:37 -0600

    Add domain objects (#51)

--- api/blueprint.py
+++ api/blueprint.py
@@ -3,14 +3,26 @@ from flask import Blueprint
 from flask_cors import CORS
 from flask_cors import cross_origin
 
-from .objects.washrooms import Washroom
-
 main_api = Blueprint('admin', __name__)
 cors = CORS(main_api)
-washroom = Washroom()
 
 
 @main_api.route("/")
 @cross_origin()
 def get_washroom_list():
-    return jsonify(washroom.get_list())
+    my_list = [
+        "Tache Hall",
+        "6th Floor E2",
+        "Aaron's House",
+        "151 Research",
+        "University Center",
+        "McDonald's Kenaston",
+        "3rd Floor Science and Technology Library",
+        "Wendy's Kenaston",
+        "Armes Tunnel Level",
+        "Eric's House",
+        "Tyler's House",
+        "University College Tunnel Level"
+    ]
+
+    return jsonify(my_list)
