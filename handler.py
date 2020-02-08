7f26e55715c964170f9fa6a16bb8565b7fa2a58f
Aaron Roller <AaronMichaelRoller@gmail.com>
2020-02-08 11:08:59 -0600

    Renamed main_api to api

--- handler.py
+++ handler.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from api.blueprint import main_api
+from api.blueprint import api
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.register_blueprint(main_api, url_prefix='/')
-
+app.register_blueprint(api, url_prefix='/')
 
 def main():
     app.run()
-
 
 if __name__ == "__main__":
     main()
