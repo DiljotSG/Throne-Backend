ededf677d3e1e6c026dc95160ba381d776baa065
Aaron Roller <AaronMichaelRoller@gmail.com>
2020-02-08 11:50:11 -0600

    Added endpoint for reviews

--- api/blueprint.py
+++ api/blueprint.py
@@ -31,47 +31,53 @@ def washrooms():
 @api.route("/washrooms/<int:washroom_id>")
 @cross_origin()
 def washrooms_id(washroom_id):
-    return "washroom: " + str(washroom_id)
+    return jsonify({"msg" : "washroom: " + str(washroom_id)})
 
 
 @api.route("/washrooms/<int:washroom_id>/reviews")
 @cross_origin()
 def washrooms_reviews(washroom_id):
-    return "reviews for washroom: " + str(washroom_id)
+    return jsonify({"msg" : "reviews for washroom: " + str(washroom_id)})
+
+
+@api.route("/reviews/<int:review_id>")
+@cross_origin()
+def reviews_id(review_id):
+    return jsonify({"msg" : "review: " + str(review_id)})
 
 
 @api.route("/buildings")
 @cross_origin()
 def buildings():
     location = request.args.get("location")
-    return "location = " + str(location)
+    return jsonify({"msg" : "building at location = " + str(location)}) 
 
 
 @api.route("/buildings/<int:building_id>")
 @cross_origin()
 def buildings_id(building_id):
-    return "building: " + str(building_id)
+    return jsonify({"msg" : "building: " + str(building_id)})
 
 
 @api.route("/buildings/<int:building_id>/washrooms")
 @cross_origin()
 def building_reviews(building_id):
-    return "washrooms in building: " + str(building_id)
+    return jsonify({"msg" : "washrooms in building: " + str(building_id)})
 
 
 @api.route("/users/<int:user_id>")
 @cross_origin()
 def user(user_id):
-    return "user: " + str(user_id)
+    return jsonify({"msg" : "user: " + str(user_id)})
 
 
 @api.route("/users/<int:user_id>/reviews")
 @cross_origin()
 def users_reviews(user_id):
-    return "reviews from user: " + str(user_id)
+    return jsonify({"msg" : "reviews from user: " + str(user_id)})
 
 
 @api.route("/users/<int:user_id>/favorites")
 @cross_origin()
 def users_favorites(user_id):
-    return "favorites of user: " + str(user_id)
+    return jsonify({"msg" : "favorites of user: " + str(user_id)})
