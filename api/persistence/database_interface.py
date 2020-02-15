e7aa7dc05fb7c26aba516afc8a9e3e5b1aa1405b
Diljot Garcha <diljotsg@gmail.com>
2020-02-15 16:02:32 -0600

    Start creating DB interfaces

---
+++ api/persistence/database_interface.py
@@ -0,0 +1,29 @@
+from abc import ABC
+
+
+class IWashrooms(ABC):
+    pass
+
+
+class IBuildings(ABC):
+    pass
+
+
+class IReviews(ABC):
+    pass
+
+
+class IRatings(ABC):
+    pass
+
+
+class IAmenities(ABC):
+    pass
+
+
+class IFavorites(ABC):
+    pass
+
+
+class IPreferences(ABC):
+    pass
