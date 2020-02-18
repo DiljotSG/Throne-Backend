c87eb03dbe46c3199dfc7305b00eb9344cb76612
Diljot Garcha <diljotsg@gmail.com>
2020-02-18 16:15:53 -0600

    Add schema validation

--- api/routes/root.py
+++ api/routes/root.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask import request
 from flask_cors import CORS
 from flask_cors import cross_origin
+from jsonschema import validate
 
 mod = Blueprint('root', __name__)
 cors = CORS(mod)
@@ -15,6 +16,43 @@ def root_data():
         "data": "Welcome to the Throne API."
     }
     return data
+
+
+@mod.route("/user")
+@cross_origin()
+def current_user():
+    event = request.environ.get("serverless.event", "no event")
+    # Get the name of the currently authenticated Cognito
+    # This is temporary, in the future we will return the user obj
+
+    # Make sure our event has the proper data - validate the schema
+    schema = {
+        "type": "object",
+        "requestContext": {
+            "type": "object",
+            "authorizer": {
+                "type": "object",
+                "claims": {
+                    "type": "object",
+                    "username": {
+                        "type": "string"
+                    }
+                }
+            }
+        }
+    }
+
+    if isinstance(event, dict):
+        validate(instance=event, schema=schema)
+        data = {
+            "username": event.requestContext.authorizer.claims.username
+        }
+        return jsonify(data)
+    else:
+        # If they aren't logged in, we can't do this.
+        return jsonify({
+            "message": "Unauthorized"
+        }), 401
 
 
 @mod.route("/debug")
