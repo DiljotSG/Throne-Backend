49ae43ec3abbca5c1da9714e0cb9f082c0cc4406
Ipdude <2389380+Ipdude@users.noreply.github.com>
2020-02-19 23:46:08 -0600

    Add mysql connection pooling to handler
    Added to requirements too

--- handler.py
+++ handler.py
@@ -1,6 +1,33 @@
 import api
+import mysql.connector
+import os
+
+config = {
+    'user': 'admin',
+    'password': os.getenv('DB_PASSWORD', ''),
+    'host': os.getenv('DB_HOSTNAME', ''),
+    'database': 'db',
+}
 
 app = api.create()
+sql_cnx = mysql.connector.connect(**config)
+
+
+def get_sql_connection():
+    global sql_cnx
+
+    if not sql_cnx:
+        # Create a new connection
+        sql_cnx = mysql.connector.connect(**config)
+        return sql_cnx
+
+    if sql_cnx.is_connected():
+        # Return current open connection
+        return sql_cnx
+
+    # Refresh old connection
+    sql_cnx = mysql.connector.connect(**config)
+    return sql_cnx
 
 
 def main():
