93c2c86fe479bc9f205c487d8ea5e6378290569f
Ipdude <2389380+Ipdude@users.noreply.github.com>
2020-02-27 23:22:11 -0600

    Add new amenities, snake case strings
    Also put them in alphabetical order... that was really annoying me before.

--- api/objects/amenity.py
+++ api/objects/amenity.py
@@ -3,23 +3,41 @@ from typing import List
 
 
 class Amenity(str, Enum):
-    PAPER_TOWEL: str = "Paper Towel"
-    AIR_DRYER: str = "Air Dryer"
-    SOAP: str = "Soap"
-    WHEELCHAIR_ACCESSIBLE: str = "Wheelchair Accessible"
-    AUTOMATIC_SINK: str = "Automatic Sink"
-    AUTOMATIC_TOILET: str = "Automatic Toilet"
-    AUTOMATIC_PAPER_TOWEL: str = "Automatic Paper Towel"
-    AUTOMATIC_DRYER: str = "Automatic Dryer"
-    SHOWER: str = "Shower"
-    URINAL: str = "Urinal"
-    PAPER_SEAT_COVERS: str = "Paper Seat Covers"
-    HYGIENE_PRODUCTS: str = "Hygiene Products"
-    NEEDLE_DISPOSAL: str = "Needle Disposal"
-    CONTRACEPTION: str = "Contraception"
-    BATHROOM_ATTENDANT: str = "Bathroom Attendant"
-    PERFUME_COLOGNE: str = "Perfume/Cologne"
-    LOTION: str = "Lotion"
+    AIR_DRYER: str = "air_dryer",
+    AIR_FRESHENER: str = "air_freshener",
+    AUTO_DRYER: str = "auto_dryer",
+    AUTO_PAPER_TOWEL: str = "auto_paper_towel",
+    AUTO_SINK: str = "auto_sink",
+    AUTO_TOILET: str = "auto_toilet",
+    BABY_CHANGE_STATION: str = "baby_change_station",
+    BABY_POWDER: str = "baby_powder",
+    BATHROOM_ATTENDANT: str = "bathroom_attendant ",
+    BIDET: str = "bidet",
+    BODY_TOWEL: str = "body_towel",
+    BODYWASH: str = "bodywash",
+    BRAILLE_LABELING: str = "braille_labeling",
+    CALL_BUTTON: str = "call_button",
+    COAT_HOOK: str = "coat_hook",
+    CONTRACEPTION: str = "contraception",
+    DIAPERS: str = "diapers",
+    HYGIENE_PRODUCTS: str = "hygiene_products",
+    FIRST_AID: str = "first_aid",
+    FULL_BODY_MIRROR: str = "full_body_mirror",
+    GARBAGE_CAN: str = "garbage_can",
+    HEATED_SEAT: str = "heated_seat",
+    LOTION: str = "lotion",
+    MOIST_TOWELETTE: str = "moist_towelette",
+    MUSIC: str = "music",
+    NEEDLE_DISPOSAL: str = "needle_disposal",
+    PAPER_SEAT_COVERS: str = "paper_seat_covers",
+    PAPER_TOWEL: str = "paper_towel",
+    PERFUME_COLOGNE: str = "perfume_cologne",
+    SAFETY_RAIL: str = "safety_rail",
+    SAUNA: str = "sauna",
+    SHAMPOO: str = "shampoo",
+    SHOWER: str = "shower",
+    TISSUES: str = "tissues",
+    WHEEL_CHAIR_ACCESS: str = "wheel_chair_access"
 
     @staticmethod
     def verify_list(amenities: List[str]):
