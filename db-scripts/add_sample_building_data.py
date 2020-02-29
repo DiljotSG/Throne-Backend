bfc58b32ee5a1b39140dd3aacefa5f1c8d3befc1
Raquel Thiessen <raquelthiessen@gmail.com>
2020-02-29 17:28:16 -0600

    Add script to add building data (#158)
    * Add script to add building data
    
    * Move it to its own file and bring back the old one
    
    * Made a quick fix
    
    * remove created_at field

---
+++ db-scripts/add_sample_building_data.py
@@ -0,0 +1,30 @@
+import json
+from api.persistence.implementations.building_impl import BuildingsPersistence
+from api.persistence.implementations.rating_impl import RatingsPersistence
+from api.objects.location import Location
+
+
+data = None
+
+b = BuildingsPersistence()
+ra = RatingsPersistence()
+
+with open('db-scripts/buildings.json') as f:
+    data = json.load(f)
+
+buildings = data["buildings"]
+
+for building in buildings:
+    best_ratings_id = ra.add_rating(0, 0, 0, 0)
+    building = b.add_building(
+        Location(
+            building["location"]["latitude"],
+            building["location"]["longitude"]
+        ),
+        building["title"],
+        building["map_service_id"],
+        building["overall_rating"],
+        best_ratings_id
+    )
+
+print("Done!")
