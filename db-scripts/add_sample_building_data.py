85a29320dc1534371d65a2a81c68acb0a7235ba3
Ipdude <2389380+Ipdude@users.noreply.github.com>
2020-02-29 20:19:10 -0600

    Add real washrooms to seed script

--- db-scripts/add_sample_building_data.py
+++ db-scripts/add_sample_building_data.py
@@ -1,13 +1,17 @@
 import json
+from api.persistence.implementations.amenity_impl import AmenitiesPersistence
 from api.persistence.implementations.building_impl import BuildingsPersistence
 from api.persistence.implementations.rating_impl import RatingsPersistence
+from api.persistence.implementations.washroom_impl import WashroomsPersistence
+from api.objects.amenity import Amenity
 from api.objects.location import Location
 
 
 data = None
-
+a = AmenitiesPersistence()
 b = BuildingsPersistence()
 ra = RatingsPersistence()
+w = WashroomsPersistence()
 
 with open('db-scripts/buildings.json') as f:
     data = json.load(f)
@@ -16,7 +20,7 @@ buildings = data["buildings"]
 
 for building in buildings:
     best_ratings_id = ra.add_rating(0, 0, 0, 0)
-    building = b.add_building(
+    buildingID = b.add_building(
         Location(
             building["location"]["latitude"],
             building["location"]["longitude"]
@@ -26,5 +30,26 @@ for building in buildings:
         building["overall_rating"],
         best_ratings_id
     )
+
+# Add two (REAL!) washrooms
+# Use the University Centre and the Elizabeth Dafoe Library
+u_centre = buildings[1]
+amenity0 = a.add_amenities(
+    Amenity.AUTO_DRYER, Amenity.PAPER_TOWEL,
+    Amenity.CONTRACEPTION, Amenity.GARBAGE_CAN
+)
+
+w.add_washroom(
+    u_centre["id"],
+    Location(
+        u_centre["location"]["latitude"],
+        u_centre["location"]["longitude"]
+    ),
+    "University Centre Main Floor Washroom",
+    2,
+    "Male",
+
+
+)
 
 print("Done!")
