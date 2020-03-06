7a1feebb231458fcd83435a5ee7a927041514707
Ipdude <2389380+Ipdude@users.noreply.github.com>
2020-03-06 16:01:22 -0600

    Organize imports again

--- api/objects/review.py
+++ api/objects/review.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from api.persistence.interfaces.rating_interface import IRatingsPersistence
-from api.persistence.interfaces.user_interface import IUsersPersistence
 from api.persistence.interfaces.preference_interface import \
     IPreferencesPersistence
+from api.persistence.interfaces.rating_interface import IRatingsPersistence
+from api.persistence.interfaces.user_interface import IUsersPersistence
 
 
 class Review:
